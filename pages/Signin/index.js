efb4b22e0582bcb8f0d04a6450d3d29571f7332c
jefersonbeckmann <jeferson.beckmann@gmail.com>
2021-09-08 20:22:39 -0300

    upload do projeto

---
+++ pages/Signin/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "react-native";
+import Header from "../../components/Header";
+import {
+  KeyboardView,
+  Title,
+  Container,
+  Input,
+  ButtonSubmit,
+  TextButton,
+} from "./styles";
+
+function Signin() {
+  return (
+    <KeyboardView>
+      <Header />
+      <Container>
+        <Title>Login</Title>
+        <Input placeholderTextColor="#fff" placeholder="E-mail" />
+        <Input
+          placeholderTextColor="#fff"
+          placeholder="Senha"
+          secureTextEntry
+        />
+        <ButtonSubmit>
+          <TextButton>Entrar</TextButton>
+        </ButtonSubmit>
+      </Container>
+    </KeyboardView>
+  );
+}
+
+export default Signin;
